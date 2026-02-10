9061bbe554efb326fe2b657ffc7a49fef0dc38e2
MR-Crisp <aka.amin2005@gmail.com>
2026-02-10 17:22:33 +0000

    started creating the input function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import pandas as pd
 
+def input_dataset(location):
+    df = pd.read_csv(location)
+    print(df.head())
+    print(df.columns)
+    print(df.shape)
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+testset = "./taxi_zone_lookup.csv"
 
+input_dataset(testset)
