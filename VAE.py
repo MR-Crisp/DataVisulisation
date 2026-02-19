aa6f404da18908ec6653b4325582b5391ed701e3
Omar Aied <omaraied007@gmail.com>
2026-02-19 12:12:47 +0000

    Increased the hidden dimensions from 64 to 128 and added more Layers to the VAE

--- VAE.py
+++ VAE.py
@@ -4,19 +4,32 @@ import torch.nn as nn
 class VariationalAutoencoder(nn.Module):
     def __init__(self, input_dim = 4, hidden_dim = 64, latent_dim = 2):#-----important the number WILL need to change, only done this for the current dataset
         super().__init__()
-        self.encoder = nn.Linear(input_dim, hidden_dim)#chaining input and hidden layers together
-        self.activation = nn.ReLU()
+        # Added more layers to the encoder to increase its capacity to learn complex representations. 
+        # Batch normalization is included to stabilize training and improve convergence.
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim // 2),
+            nn.BatchNorm1d(hidden_dim // 2),
+            nn.ReLU()
+        )
+        encoder_outut_dim = hidden_dim // 2
 
-        self.fc_mu = nn.Linear(hidden_dim, latent_dim)#this is for the mean
-        self.fc_logvar = nn.Linear(hidden_dim, latent_dim)#this is for the variance in the normal distribution
-        self.decoder = nn.Sequential(nn.Linear(latent_dim, hidden_dim),nn.ReLU(),nn.Linear(hidden_dim, input_dim))
-        """
-        This is encoder but in reverse. First layer is connected expanding latent to hidden using ReLu
-        second layer is for connection between hidden and 'input' layer and uses sigmoid to expand.
-        sigmoid might need to be changed    
-        """
+        self.fc_mu = nn.Linear(encoder_outut_dim, latent_dim)#this is for the mean
+        self.fc_logvar = nn.Linear(encoder_outut_dim, latent_dim)#this is for the variance in the normal distribution
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim // 2),
+            nn.BatchNorm1d(hidden_dim // 2),
+            nn.ReLU(),
+            nn.Linear(hidden_dim // 2, input_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, input_dim)
+            )
+
     def encode(self, x):
-        h = self.activation(self.encoder(x))#passes x into input layer, and then h is returned from hidden layer
+        h = self.encoder(x)#passes x into input layer, and then h is returned from hidden layer
         return self.fc_mu(h), self.fc_logvar(h)# using h we then get the mean and variace (latent layer)
 
     def reparameterize(self, mu, logvar):# allows us to sample a latent space
