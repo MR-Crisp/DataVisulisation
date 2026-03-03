854991f792ce3f7c74e1812d699d3e51439e37c1
Tama Guisado Okumura <tama.guisado.okumura@gmail.com>
2026-03-03 18:27:21 +0000

    Base for frontend and new branch

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-5 bg-[#F0EBE1]">
+
+      {/* Top bar*/}
+      <div className="mb-5">
+        <h1 className="font-bold text-2xl text-[#0D0D0D]">Welcome Back</h1>
+      </div>
+
+      {/* Grid: auto columns and rows sized at 1fr each */}
+      <div className="grid grid-cols-[repeat(4,1fr)] grid-rows-[repeat(4,1fr)] gap-4 h-[600px]">
+
+        {/* Place your blocks here — example placeholders */}
+        <div className="bg-[#E8E0D0] rounded-xl col-span-2 row-span-4 text-center flex items-center justify-center">File area</div>
+        <div className="bg-[#C8B4A0] rounded-xl col-span-2 row-span-4 text-center flex items-center justify-center">Graph Area</div>
+
+      </div>
+    </main>
+  );
+}
