396f000c9b3427665907c23fda667ec2f850b976
Tama Guisado Okumura <tama.guisado.okumura@gmail.com>
2026-03-31 17:12:01 +0100

    Api_model.py code update

--- api_model.py
+++ api_model.py
@@ -1,18 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import plotly.graph_objects as go
-from main import main
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get("/graph")
-def get_graph():
-    fig = main()
-    return fig.to_json()
+@app.get("/")
+def root():
+    return {"message": "Welcome to the API!"}
