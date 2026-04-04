d0aab067105ef39f0165ac246cc8a77c9103cf15
MR-Crisp <aka.amin2005@gmail.com>
2026-04-04 22:17:34 +0100

    creating the skeleton for future api

--- api_model.py
+++ api_model.py
@@ -1,18 +1,38 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import plotly.graph_objects as go
-from main import main
+import pandas as pd
+import io
+from fastapi import UploadFile, File
+from voronoi_algorithm import voronoi_finite_polygons,plot_voronoi
+import umap
 
 app = FastAPI()
+df =  None # main df for the dataset
+latent_space = None #from VAE and dataset
+@app.get("/")
+def root():
+    return {"message": "Welcome to the API!"}
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.post("Upload_CSV")
+async def upload_csv(file: UploadFile = File(...)):
+    contents = await file.read()
+    df = pd.read_csv(io.BytesIO(contents))
 
-@app.get("/graph")
-def get_graph():
-    fig = main()
-    return fig.to_json()
+@app.post("/data_cleaning")
+def data_cleaning(df: pd.DataFrame):
+    pass
+
+@app.post("/vae_GMM")
+def vae(df: pd.DataFrame):
+    pass
+
+@app.get("/voronoi")
+def voronoi(lanent_space):
+    pass
+
+@app.get("/heatmap")
+def heatmap(latent):
+    pass
+
+@app.get("particle")
+def particle(latent):
+    pass
