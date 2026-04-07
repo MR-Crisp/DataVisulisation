1ac087b270b8f803d08c59b2e7fdd92ab5fa4799
Tama Guisado Okumura <tama.guisado.okumura@gmail.com>
2026-04-07 12:54:29 +0100

    File Uploader Component and Integration

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,23 +1,8 @@
 "use client";
 
-import dynamic from 'next/dynamic';
-import { useState, useEffect } from 'react';
+import FileUploader from "@/components/FileUploader";
 
-const Plot = dynamic(() => import('react-plotly.js'), { ssr: false,loading: () => <p>Loading Chart...</p> });
-
-export default function Chart3D() {
-  const [figure, setFigure] = useState<any>(null);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/graph")
-      .then(res => res.json())
-      .then(json => {
-        const parsed = typeof json === 'string' ? JSON.parse(json) : json;
-        setFigure(parsed);
-      });
-  }, []);
-  
-  if (!figure) return <p>Loading...</p>;
+export default function App() {
 
   return (
     <main className="min-h-screen p-5 bg-[#F0EBE1]">
@@ -26,24 +11,23 @@ export default function Chart3D() {
       <div className="mb-5 flex items-center">
         <h4 className="font-bold text-sm text-[#0D0D0D]">Welcome Back</h4>
         <h1 className="font-bold text-xl text-[#0D0D0D] absolute left-1/2 -translate-x-1/2">Data Visualisation</h1>
-
       </div>
 
- 
-      <div className="grid grid-cols-[repeat(4,1fr)] grid-rows-[repeat(4,1fr)] gap-4 h-[600px]">
+
+      <div className="grid grid-cols-[1fr_3fr] gap-4 h-[560px]">
 
       {/* Grid: auto columns and rows sized at 1fr each */}
-
-
-        <div className="bg-[#E8E0D0] rounded-xl col-span-2 row-span-4 text-center flex items-center justify-center">File area</div>
-
+        <div className="flex flex-col gap-3">
+        <div className="flex-1 bg-[#E8E0D0] rounded-xl border-2 border-[#0D0D0D] flex flex-col items-center justify-center gap-2 text-[#7A7060]">
+          <span className="text-sm">Upload File Here</span>
+          <FileUploader/>
+        </div>
+        <button className="bg-[#0D0D0D] text-[#F0EBE1] py-2 px-4 rounded-lg hover:bg-[#333]">Upload File</button>
+        </div>
 
       {/* Graphs*/}
-        <div className="bg-[#C8B4A0] rounded-xl col-span-2 row-span-4 text-center flex items-center justify-center">
-          <Plot
-          data={figure.data}
-          layout={figure.layout}
-        />
+        <div className="bg-[#C8B4A0] rounded-xl flex items-center justify-center">
+          <button className="bg-[#0D0D0D] text-[#F0EBE1] py-2 px-4 rounded-lg hover:bg-[#333]">Update Graph</button>
        </div>
       </div>
     </main>
